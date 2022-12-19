0aea848c96ca110f368e556b41e2acd291884c20
Evgeny <zenka712@mail.ru>
2022-12-19 17:10:39 +0300

    FIrst commit, added test and build preferences

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+/**
+ * @brief Test class for Doxygen
+ * 
+ */
+class TestClass {
+    public:
+        int variable;
+        string second_variable;
+};
+
+
+/**
+ * @brief Simple test cpp main function
+ * 
+ * @return int 
+ */
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
---
+++ tests/test_1.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+
+
+/**
+ * @brief Construct a new TEST object
+ * 
+ */
+TEST(Example, EmptyTest) {
+    EXPECT_TRUE(true);
+}
